321e5d8fe745fd528aab8322c21490b34eb2b746
tkhuyen95 <khuyen.chau1995@gmail.com>
2025-07-09 13:38:49 +0700

    update

---
+++ src/main/java/org/example/BaiTap1Sao/TinhTongCacSoChan.java
@@ -0,0 +1,24 @@
+package org.example.BaiTap1Sao;
+
+import java.util.Scanner;
+
+public class TinhTongCacSoChan {
+    public static int tongChan(int n){
+        int tong = 0;
+        //duyệt qua vòng lặp
+        for (int i = 0; i <= n; i+=2){
+            tong += i;
+        }
+        return tong;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhập số n: ");
+        int n = scanner.nextInt();
+
+        int result = tongChan(n);
+        System.out.println("Tổng chẵn: " + result);
+    }
+}
